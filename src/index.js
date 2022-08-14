b0806a02b6ab207170b0dc47ecb5248793469414
devansh-jain-2002 <jdevansh21@iitk.ac.in>
2022-08-14 18:47:53 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
 
-app.get('/users',(req,res)=>{
+app.get('/students',(req,res)=>{
     Student.find(
         {"name":{$regex : req.body.name?req.body.name:""},
         "roll":{$regex : req.body.roll?req.body.roll:""},
         "userID":{$regex : req.body.userID?req.body.userID:""},
-        "branch":{$regex : req.body.branch?req.body.branch:""},}).then((users)=>{
-        res.send(users)
+        "branch":{$regex : req.body.branch?req.body.branch:""},}).then((students)=>{
+        res.send(students)
     }).catch((e)=>{
         res.status(500).send()
     })
 })
-app.post('/users',(req,res)=>{
+app.post('/students',(req,res)=>{
     const student = new Student(req.body)
     student.save().then(()=>{
         res.send(student)
@@ -24,12 +24,12 @@ app.post('/users',(req,res)=>{
         res.status(400).send(e)
     })
 })
-app.patch('/users/:roll',(req,res)=>{
+app.patch('/students/:roll',(req,res)=>{
     Student.findOneAndUpdate(req.params,req.body,{new:true}).then((user)=>{
         res.send(user)
     })
 })
-app.delete('/users/:roll',(req,res)=>{
+app.delete('/students/:roll',(req,res)=>{
     Student.deleteOne(req.params).then(()=>{
         res.send("Deleted Successfully")
     })
